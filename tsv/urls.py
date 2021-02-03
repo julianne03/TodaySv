ca0a7fbae1a91cc43982dceb9b5a73533cc5fab3
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-03 15:56:26 +0900

    #1-2. first function url mapping

--- tsv/urls.py
+++ tsv/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from tsv import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tsv/', include('tsv.urls')),
+    path('', views.index)
 ]
