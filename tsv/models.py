179d09e25dd2fdc39bcc91db5561dfe374741b62
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-04 21:52:16 +0900

    #1-5. answer model - weather testing

--- tsv/models.py
+++ tsv/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Answers(models.Model) :
+
+    WEATHER_CHOICES = {
+        ('sunny', 'images/sunny'),
+        ('cloudy', 'images/cloudy'),
+        ('windy', 'images/windy'),
+        ('rainy', 'images/rainy'),
+        ('snowy', 'images/snowy')
+    }
+
+    weather = models.CharField(max_length=80, choices=WEATHER_CHOICES, null=True)
