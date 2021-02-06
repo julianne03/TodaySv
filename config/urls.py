a09e51ca4e66fe024da3be1e7fc8c81a65ca8d9d
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-07 00:07:25 +0900

    #1-15. signup success!

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from tsv import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('tsv/', include('tsv.urls')),
     path('common/', include('common.urls')),
