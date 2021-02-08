5ab65882dda118d77c4648ec70562e3d1ced5d4c
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-08 13:07:08 +0900

    #2-3. Answers model modify second

--- tsv/models.py
+++ tsv/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Answers(models.Model) :
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    answer_date = models.DateTimeField()
+    answer_date = models.CharField(max_length=30)
     cm_answer_date = models.DateTimeField(null=True)
     weather = models.CharField(max_length=80, null=True)
     mood = models.CharField(max_length=80, null=True)
-    wake_up = models.DateTimeField(null=True)
+    wake_up = models.CharField(max_length=30, null=True)
     did_well = models.CharField(max_length=100, null=True)
-    happiness = models.TextField(max_length=100, null=True)
+    happiness = models.CharField(max_length=100, null=True)
     breakfast = models.CharField(max_length=100, null=True)
     lunch = models.CharField(max_length=100, null=True)
     dinner = models.CharField(max_length=100, null=True)
