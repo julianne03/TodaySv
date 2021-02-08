38a229ba007172c47849a29caec1dda134d9de7d
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-08 13:11:39 +0900

    #2-5. answers - weather clear!

--- tsv/views.py
+++ tsv/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Answers
-from .forms import AnswersForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from django.utils import timezone
 
+from .models import Answers
+from .forms import WeatherForm
+
+@login_required(login_url='common:login')
 def index(request) :
     return render(request, 'tsv/index.html')
 
-def answers_create(request) :
-    if request.method == 'POST' :
-        form = AnswersForm(request.POST)
-        if form.is_valid() :
-            answers = form.save(commit=False)
-            answers.weather = request.GET()
-            answers.save()
-            return redirect('tsv:index')
+@login_required(login_url='common:login')
+def answers_weather(request) :
+    #만약 해당하는 데이터가 없다면 밑에 실행 아니면 else -> 함수로 따로 빼기
+    date = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}'
+    answer, is_exists = Answers.objects.get_or_create(username_id=request.user.id, answer_date=date)
+    if request.method == 'POST':
+        form = WeatherForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.cm_answer_date = timezone.now()
+            answer.save()
+            return redirect('tsv:answers_mood')
     else:
-        form = AnswersForm()
-    context = {'form':form}
-    return render(request, 'tsv/answers_form.html', context)
+        form = WeatherForm(instance=answer)
+    context = {'form': form}
+    return render(request, 'tsv/weather_form.html', context)
