1140ea1673fa1c8c7953eecd360a13344698c9f3
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-13 17:05:14 +0900

    #3-3. backend setting again for deploy

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import my_settings
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,8 +63,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
